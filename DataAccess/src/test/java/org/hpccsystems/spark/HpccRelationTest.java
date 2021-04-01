485fff48934927566d82f622653e29f1bd9e9287
Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexisrisk.com>
2021-04-01 15:59:02 -0400

    JAPI-397 Enable Spark remote test
    Signed-off-by: Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexisrisk.com>

--- DataAccess/src/test/java/org/hpccsystems/spark/HpccRelationTest.java
+++ DataAccess/src/test/java/org/hpccsystems/spark/HpccRelationTest.java
@@ -2,8 +2,14 @@ package org.hpccsystems.spark;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeMap;
 
+import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
+import org.apache.spark.rdd.RDD;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.sources.EqualTo;
 import org.apache.spark.sql.sources.Filter;
 import org.apache.spark.sql.sources.GreaterThan;
@@ -22,6 +28,7 @@ import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 public class HpccRelationTest extends BaseRemoteTest
 {
@@ -35,6 +42,11 @@ public class HpccRelationTest extends BaseRemoteTest
     @BeforeClass
     public static void setUpBeforeClass() throws Exception
     {
+        if (System.getProperty("sparkmaster") == null)
+            System.out.println("sparkmaster not provided - defaulting to 'spark://localhost:7077'");
+        else
+            System.out.println("sparkmaster: '" + sparkMaster + "'");
+
         supportedSparkFilters.add(new StringStartsWith("fixstr8", "Rod"));
         supportedSparkFilters.add(new Or(new LessThan("int8", 12), new GreaterThan("int8", 8)));
         supportedSparkFilters.add(new In("int8", new Object [] { "str", "values", "etc"}));
@@ -51,36 +63,39 @@ public class HpccRelationTest extends BaseRemoteTest
         unsupportedSparkFilters.add(new StringContains("somestring", "some"));
         unsupportedSparkFilters.add(new StringEndsWith("somestring", "ing"));
 
-//        TreeMap<String, String> paramTreeMap = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-//        paramTreeMap.put("host", connString);
-//        paramTreeMap.put("path", DEFAULTHPCCFILENAME);
-//        paramTreeMap.put("cluster", thorClusterFileGroup);//thor_160
-//        paramTreeMap.put("username", hpccUser);
-//        paramTreeMap.put("password", hpccPass);
-//
-//        hpccopts = new HpccOptions(paramTreeMap);
-//        SparkConf sparkconf = new SparkConf();
-//        sparkconf.setMaster(sparkMaster).setAppName("SPARKHPCCJUnit");
-//        sparkcontext = new SparkContext(sparkconf);
+        TreeMap<String, String> paramTreeMap = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+        paramTreeMap.put("host", connString);
+        paramTreeMap.put("path", DEFAULTHPCCFILENAME);
+        paramTreeMap.put("cluster", thorClusterFileGroup);//thor_160
+        paramTreeMap.put("username", hpccUser);
+        paramTreeMap.put("password", hpccPass);
+
+        hpccopts = new HpccOptions(paramTreeMap);
+        Assert.assertNotNull("HPCC Opts object == null!", hpccopts);
+        System.out.println("HPCC Opts: " + hpccopts.toString());
+        SparkConf sparkconf = new SparkConf();
+        Assert.assertNotNull("SparkConf object == null!", sparkconf);
+        sparkconf.setMaster(sparkMaster).setAppName("SPARKHPCCJUnit");
+        sparkcontext = new SparkContext(sparkconf);
     }
 
-//    @Test
-//    @Category(org.hpccsystems.commons.annotations.RemoteTests.class)
-//    public void testbuildScanAllValid() throws Exception
-//    {
-//        Assume.assumeTrue("Supported Spark Filters not available", supportedSparkFilters.size() > 0);
-//        Assume.assumeTrue("Unsupported Spark Filters not available", unsupportedSparkFilters.size() > 0);
-//        Assume.assumeTrue("Spark context not set!", sparkcontext != null);
-//        SparkSession sparksession = new SparkSession(sparkcontext);
-//        SQLContext sqlcontext = new SQLContext(sparksession);
-//
-//        Assume.assumeTrue("Spark SQL context not set!", sqlcontext != null);
-//        Assume.assumeTrue("hpccopts not set!", hpccopts != null);
-//
-//        System.out.println("Testing buildScanallvalid");
-//        HpccRelation hpccRelation = new HpccRelation(sqlcontext, hpccopts);
-//        RDD<Row> rdd = hpccRelation.buildScan(new String[]{"int8"}, supportedSparkFilters.toArray(new Filter[0]));
-//    }
+    @Test
+    @Category(org.hpccsystems.commons.annotations.RemoteTests.class)
+    public void testbuildScanAllValid() throws Exception
+    {
+        Assume.assumeTrue("Supported Spark Filters not available", supportedSparkFilters.size() > 0);
+        Assume.assumeTrue("Unsupported Spark Filters not available", unsupportedSparkFilters.size() > 0);
+        Assume.assumeTrue("Spark context not set!", sparkcontext != null);
+        SparkSession sparksession = new SparkSession(sparkcontext);
+        SQLContext sqlcontext = new SQLContext(sparksession);
+
+        Assume.assumeTrue("Spark SQL context not set!", sqlcontext != null);
+        Assume.assumeTrue("hpccopts not set!", hpccopts != null);
+
+        System.out.println("Testing buildScanallvalid");
+        HpccRelation hpccRelation = new HpccRelation(sqlcontext, hpccopts);
+        RDD<Row> rdd = hpccRelation.buildScan(new String[]{"int8"}, supportedSparkFilters.toArray(new Filter[0]));
+    }
 
     @Test
     public void testUnhandledFiltersAllValid() throws Exception
